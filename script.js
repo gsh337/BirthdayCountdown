8a61e7d896efe2d71e96c99d535a6b7d130b9d10
prosto_gasha <73956556+prostogasha@users.noreply.github.com>
2023-06-25 01:30:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -18,9 +18,81 @@ var countdown = setInterval(function () {
     var countdownText = days + ' ' + daysUnit + ', ' + hours + ' ' + hoursUnit + ', ' + minutes + ' ' + minutesUnit + ', ' + seconds + ' ' + secondsUnit;
     document.getElementById('countdown').innerHTML = countdownText;
 
+    function startConfettiAnimation() {
+        const colors = ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"];
+        const shapes = ["circle", "square", "triangle"];
+
+        const duration = 15 * 1000,
+            animationEnd = Date.now() + duration,
+            defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+        function randomInRange(min, max) {
+            return Math.random() * (max - min) + min;
+        }
+
+        const interval = setInterval(function () {
+            const timeLeft = animationEnd - Date.now();
+
+            if (timeLeft <= 0) {
+                clearInterval(interval);
+                startConfettiAnimation(); // Restart the confetti animation
+                return;
+            }
+
+            confetti(
+                Object.assign({}, defaults, {
+                    particleCount: 4,
+                    shapes: shapes,
+                    colors: colors,
+                    origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+                })
+            );
+
+            confetti(
+                Object.assign({}, defaults, {
+                    particleCount: 4,
+                    scalar: 1.8,
+                    shapes: ["text"],
+                    shapeOptions: {
+                        text: {
+                            value: ["🎂", "🎉", "🎈", "🎁", "🎊", "🍰", "🎇", "🥳", "🎵", "🎶"],
+                        }
+                    },
+                    origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+                })
+            );
+
+            confetti(
+                Object.assign({}, defaults, {
+                    particleCount: 4,
+                    scalar: 1.8,
+                    shapes: ["text"],
+                    shapeOptions: {
+                        text: {
+                            value: ["🎂", "🎉", "🎈", "🎁", "🎊", "🍰", "🎇", "🥳", "🎵", "🎶"],
+                        }
+                    },
+                    origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+                })
+            );
+
+            confetti(
+                Object.assign({}, defaults, {
+                    particleCount: 4,
+                    shapes: shapes,
+                    colors: colors,
+                    origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+                })
+            );
+        }, 250);
+    }
+
+    // Call the startConfettiAnimation() function when the countdown reaches zero
     if (distance < 0) {
         clearInterval(countdown);
         document.getElementById('countdown').innerHTML = 'It\'s your birthday! 🎉';
-        document.getElementById('birthdayQuote').innerHTML = randomQuote;
+
+        // Start the confetti animation
+        startConfettiAnimation();
     }
 }, 1000);
